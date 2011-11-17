cd0f26b6e8c4e248bc130ffa7225dc4a09205bd6
Ryan Lane <rlane@wikimedia.org>
2011-11-17 16:31:28 -0600

    Adding fake contacts.pp manifest
    Change-Id: I4e24fd3d99769c3c8a899254d00cbb7cf3890c2e

---
+++ manifests/contacts.pp
@@ -0,0 +1,8 @@
+class contacts::udp2log {
+	$vrije_universiteit_contact = ""
+	$universidad_rey_juan_carlos_contact = ""
+	$universidad_rey_juan_carlos_backup_contact = ""
+	$university_minnesota_contact = ""
+	$university_minnesota_contact_former = ""
+	$university_minnesota_contact_former2 = ""
+}
