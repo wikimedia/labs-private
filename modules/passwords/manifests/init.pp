f2901768dec574d6c89d9042d7eb4758d3114417
Alexandros Kosiaris <akosiaris@wikimedia.org>
2017-07-05 15:14:56 +0300

    Remove unused ores_password
    Change-Id: I37021bf4825ab48a7c3c502e1427ce1c11727289

--- modules/passwords/manifests/init.pp
+++ modules/passwords/manifests/init.pp
@@ -266,7 +266,6 @@ class passwords::mysql::wikimania_scholarships {
 
 class passwords::redis {
     $main_password = 'Q6dzak4k9vjYjh341fHS'
-    $ores_password = 'seagullsoup'
 }
 
 class passwords::geoip {
